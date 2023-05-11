dd7279f75d8f4f72f6e642f4150c6552622fc17f
Arley Souza <souzaarley12@gmail.com>
2023-05-11 09:37:09 -0300

    conclusão sem interface

--- AluguelCarros/Services/BrazilTaxService.cs
+++ AluguelCarros/Services/BrazilTaxService.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AluguelCarros.Services
+﻿namespace AluguelCarros.Services
 {
     internal class BrazilTaxService
     {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 02;
+
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
     }
 }
